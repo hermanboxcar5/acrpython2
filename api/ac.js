4831137cb06ed19284a015ffc70442909c711c0e
hermanboxcar5 <95950881+hermanboxcar5@users.noreply.github.com>
2024-02-25 16:03:23 -0800

    Create ac.js

---
+++ api/ac.js
@@ -0,0 +1,14 @@
+const { exec } = require('child_process');
+
+module.exports = (req, res) => {
+    exec('python ac.py', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+        res.status(200).json({ message: 'Script executed successfully.' });
+    });
+};
