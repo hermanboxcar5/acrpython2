5791c82e1c5eed4befab5472561602fd9915ff20
hermanboxcar5 <95950881+hermanboxcar5@users.noreply.github.com>
2024-02-26 19:10:07 -0800

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,27 @@
 import os
 import requests
+import time
 import json
+import threading
+
+from flask import Flask
+tf=True
+app = Flask(__name__)
+
 # Credentials
 username = os.environ.get('username')
 password = os.environ.get('password')
+
+last_request_timestamp = None
+
+def claim_timed_bonus():
+    global last_request_timestamp
+    while True:
+        response = authenticate_user()
+        token = json.loads(response.content)['token']
+        response = collect_timed_bonus(token)
+        last_request_timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+        time.sleep(1801)  # Sleep for approximately 30 minutes
 
 def authenticate_user():
     response = requests.post(
@@ -29,6 +47,20 @@ def collect_timed_bonus(token):
         data=payload.encode('utf-8'))
     return response
 
-response = authenticate_user()
-token = json.loads(response.content)['token']
-response = collect_timed_bonus(token)
+@app.route('/')
+def last_request_time():
+    if tf:
+        tf=False
+        # Start a background thread to claim timed bonus
+        claim_timed_bonus_thread = threading.Thread(target=claim_timed_bonus)
+        claim_timed_bonus_thread.start()
+    global last_request_timestamp
+    if last_request_timestamp:
+        return f"Last request sent at: {last_request_timestamp}"
+    else:
+        return "No requests sent yet."
+
+
+
+    # Run the Flask app
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
